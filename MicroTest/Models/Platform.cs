f7fe3298a71f22613b9be225781fc74e07670033
mohsenasadi501 <mohsenasadi501@outlook.com>
2023-10-19 12:12:53 +0330

    Add project files.

---
+++ MicroTest/Models/Platform.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroTest.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string cost { get; set; }
+    }
+}
