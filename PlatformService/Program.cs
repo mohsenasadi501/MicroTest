e532f18aca816694068e12192834f8143aa0fb2b
mohsenasadi501 <mohsenasadi501@outlook.com>
2023-10-27 22:53:33 +0330

    Change the Microservice Name

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 PrepDb.PrepPopulation(app);
 
