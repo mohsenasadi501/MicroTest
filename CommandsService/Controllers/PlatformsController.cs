57fc468e9c1b46b17d18343c167c5827a237f367
mohsenasadi501 <mohsenasadi501@yahoo.com>
2023-11-05 20:20:03 +0330

    Add Commands microservice to the project

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
